45479520eee38232d2c97d22485e01749a07ede8
justcoding121 <jehonathant@gmail.com>
2018-04-24 14:45:44 -0400

    longest consecutive ones with k flips iterative

--- CS.Problems.Tests/Miscellaneous/LongestOnesWithKFlips_Tests.cs
+++ CS.Problems.Tests/Miscellaneous/LongestOnesWithKFlips_Tests.cs
@@ -9,20 +9,25 @@ using System.Threading.Tasks;
 
 namespace CS.Problems.Tests.Miscellaneous
 {
-    /// <summary>
-    /// Problem details below
-    /// http://www.geeksforgeeks.org/counting-inversions/
-    /// </summary>
+
     [TestClass]
     public class LongestOnesWithKFlips_Tests
     {
         [TestMethod]
         public void LongestOnesWithKFlips_Smoke_Test()
         {
-           Assert.AreEqual(7, LongestOnesWithKFlips.ComputeR("11001100111", 2));
-           Assert.AreEqual(8, LongestOnesWithKFlips.ComputeR("10011010111", 2));
-           Assert.AreEqual(5, LongestOnesWithKFlips.ComputeR("10011010111", 1));
-           Assert.AreEqual(4, LongestOnesWithKFlips.ComputeR("0001", 4));
+
+            //recursive solution tests
+            Assert.AreEqual(7, LongestOnesWithKFlips.ComputeRecursive("11001100111", 2));
+            Assert.AreEqual(8, LongestOnesWithKFlips.ComputeRecursive("10011010111", 2));
+            Assert.AreEqual(5, LongestOnesWithKFlips.ComputeRecursive("10011010111", 1));
+            Assert.AreEqual(4, LongestOnesWithKFlips.ComputeRecursive("0001", 4));
+
+            //Iterative solution tests
+            Assert.AreEqual(7, LongestOnesWithKFlips.ComputeIterative("11001100111", 2));
+            Assert.AreEqual(8, LongestOnesWithKFlips.ComputeIterative("10011010111", 2));
+            Assert.AreEqual(5, LongestOnesWithKFlips.ComputeIterative("10011010111", 1));
+            Assert.AreEqual(4, LongestOnesWithKFlips.ComputeIterative("0001", 4));
 
         }
     }
--- CS.Problems/Miscellaneous/LongestOnesWithKFlips.cs
+++ CS.Problems/Miscellaneous/LongestOnesWithKFlips.cs
@@ -7,8 +7,8 @@ namespace CS.Problems.Miscellaneous
 {
     public class LongestOnesWithKFlips
     {
-        //k*n time complexity & k space complexity
-        public static int ComputeR(string a, int k)
+        //k*n time complexity & k space complexity recursive approach
+        public static int ComputeRecursive(string a, int k)
         {
             var max = 0;
 
@@ -40,5 +40,50 @@ namespace CS.Problems.Miscellaneous
 
             return Compute(a, k - 1, i - 1) + 1;
         }
+
+        //O(n) time complexity iterative approach
+        public static int ComputeIterative(string a, int k)
+        {
+            var c = a.ToCharArray();
+            int left = 0, right = 0;
+            int max = 0;
+
+            //fill first k 0'z with 1's creating a window
+            while(k>0 && right < a.Length)
+            {
+                if (c[right] == '1')
+                {
+                    right++;
+                }
+                else
+                {
+                    k--;
+                    right++;
+                }
+            }
+            max = Math.Max(max, right - left);
+
+            //slide window till end updating new maximum
+            while (right < a.Length)
+            {
+                if (c[right] == '1')
+                {
+                    right++;
+                }
+                else
+                {
+                    while (c[left] == '1')
+                        left++;
+
+                    left++;
+                    right++;
+                }
+
+                max = Math.Max(max, right - left);
+            }
+
+            return max;
+        }
+
     }
 }
