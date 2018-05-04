ee662ad8c5df8ac65897dab12ecb175de23921ff
justcoding121 <jehonathant@gmail.com>
2018-05-03 22:14:52 -0400

    moved expression tree from advanced algorithms repo

---
+++ CS.Problems.Tests/Miscellaneous/ExpressionTree_Tests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CS.Problems.Miscellaneous
+{
+    [TestClass]
+    public class ExpressionTree_Tests
+    {
+        [TestMethod]
+        public void ExpressionTree_Smoke_Test()
+        {
+            var postfixInput = "ab+ef*g*-".ToCharArray();
+            var tree = new ExpressionTree<char>();
+
+            tree.Construct(postfixInput, new char[] { '+', '-', '*', '/' });
+
+            var infixResult = tree.GetInfix();
+            var expected = "a+b-e*f*g";
+
+            for (int i = 0; i < infixResult.Count; i++)
+            {
+                Assert.IsTrue(expected[i].Equals(infixResult[i]));
+            }
+        }
+    }
+}
